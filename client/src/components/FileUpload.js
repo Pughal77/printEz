58038d83b5ac69128ed8367f0cdc01b974c95811
jayllo-c <jaylotc@gmail.com>
2023-06-19 12:21:59 +0800

    Now checks if file uploaded is a pdf file
    displays a warning upon wrong filetype being uploaded.
    - bugs were rectified before this commit was made

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import { ThemeProvider } from '@emotion/react';
 
 import { theme } from '../utils/theme'
@@ -10,19 +12,30 @@ import { theme } from '../utils/theme'
 function FileUpload({ isUploaded, setIsUploaded, socket }) {
     // variable to store selected file
     const [selectedFile, setSelectedFile] = useState();
+    const [isPDF, setIsPDF] = useState(false);
+    const [pdfWarning, setPDFWarning] = useState(false);
 
     const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0]);
+        if (e.target.files[0] != null) {
+            const currFile = e.target.files[0];
+            if (currFile.type == "application/pdf") {
+                setIsPDF(true);
+                setSelectedFile(currFile);
+            }
+        }
     }
 
     const handleUpload = async(e) => {
         e.preventDefault();
+        if (isPDF) {
+            console.log(selectedFile);
         
-        console.log(selectedFile);
-        
-        socket.emit("pdfTransfer", selectedFile, (status) => {
-            console.log(status);
-        });
+            socket.emit("pdfTransfer", selectedFile, (status) => {
+                console.log(status);
+            });
+        } else {
+            setPDFWarning(true);
+        }
     }
 
     // acts as a listner for socket events
@@ -30,10 +43,13 @@ function FileUpload({ isUploaded, setIsUploaded, socket }) {
         socket.on("missingCredentials", () => {
             // force the page to reload, credentials misssing
             window.location.reload(false);
+            setPDFWarning(false);
+            setIsPDF(false);
         })
         socket.on("fileUploaded", () => {
             console.log("FILE UPLOADED");
             setIsUploaded(true);
+            setIsPDF(false);
         })
     }, [socket]) 
 
@@ -85,6 +101,15 @@ function FileUpload({ isUploaded, setIsUploaded, socket }) {
                         </Button>
                     </form>
                 </Box>
+                {pdfWarning && 
+                    <Alert 
+                        severity="error"
+                        onClose={() => {setPDFWarning(false)}}
+                    >
+                        <AlertTitle>ERROR</AlertTitle>
+                        <strong>please upload a PDF file</strong>
+                    </Alert>
+                }
             </Container>
         </ThemeProvider>
      );
