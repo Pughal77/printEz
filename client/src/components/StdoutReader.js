93ca65388c44a0205553c2fcb3ea3640284a2249
tamago55 <pughalraj77@gmail.com>
2023-06-23 12:30:10 +0800

    first of many

---
+++ client/src/components/StdoutReader.js
@@ -0,0 +1,10 @@
+function read (stdout) {
+    // returns an array of print job objects
+    result = []
+    // iterate through the string and see for every first time u see a white space and fill out the
+    // fields of the printQ objects respectively
+    for (let i = 245; i < stdout.length; i++) {
+    }
+}
+
+read("")
