4094d93c202897b64c4f809dbc46fbb08b5fb6c0
jayllo-c <jaylotc@gmail.com>
2023-07-19 15:13:32 +0800

    mini changes

--- client/src/components/dataGrid.js
+++ client/src/components/dataGrid.js
@@ -2,64 +2,43 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+import { theme } from '../style/style';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', headerName: 'No.', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
+    field: 'fileName',
+    headerName: 'File name',
+    width: 300,
     editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 150
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 150,
-    // see how to render a delete button here!
-    // renderCell: (params) => {return (<Button>lol</Button>)}
   }
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, fileName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, fileName: 'Lannister', firstName: 'Cersei', age: 42 }
 ];
 
 export default function DataGridDemo() {
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: 400, width: '100%', backgroundColor: 'primary.main' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-      />
-    </Box>
+          }}
+          pageSizeOptions={[5]}
+        />
+      </Box>
+    </ThemeProvider>
+    
   );
 }
